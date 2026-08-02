f758c29636ba30f67290f9ebe51197621fb28eb9
a0527198150-del <a0527198150@gmail.com>
2026-08-02 20:27:06 +0300

    Update Color.kt

--- app/src/main/java/com/example/ui/theme/Color.kt
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -26,3 +26,15 @@ val MinimalistNavBg = Color(0xFFF3F4F9)
 val MinimalistTabActive = Color(0xFFE1E2EC)
 val MinimalistTabInactive = Color.Transparent
 val MinimalistRowHover = Color(0xFFF0F0F5)
+
+// Dark theme variants of the Clean Minimalism palette
+val MinimalistDarkBg = Color(0xFF101418)
+val MinimalistDarkSurface = Color(0xFF1A1F24)
+val MinimalistDarkTextPrimary = Color(0xFFE2E2E6)
+val MinimalistDarkTextSecondary = Color(0xFFB8BDC7)
+val MinimalistDarkAccent = Color(0xFF7FCFFF)
+val MinimalistDarkContainer = Color(0xFF00344F)
+val MinimalistDarkOnContainer = Color(0xFFCFE5FF)
+val MinimalistDarkBorder = Color(0xFF3A3F45)
+val MinimalistDarkLightBorder = Color(0xFF2A2F35)
+val MinimalistDarkNavBg = Color(0xFF171C21)
